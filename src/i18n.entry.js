ade3d557c3e293736b8d94cc1b527088f8e1a233
Arthur Li <34063465+FFFFourwood@users.noreply.github.com>
2023-03-12 01:17:34 +0800

    准备增加国际化功能

---
+++ src/i18n.entry.js
@@ -0,0 +1,59 @@
+const entries = {
+    command_commandHandlers_help: {
+        zh: "获取命令帮助",
+        en: "Get command help"
+    },
+    command_commandHandlers_new: {
+        zh: "发起新的对话",
+        en: "Start a new conversation."
+    },
+    command_commandHandlers_start: {
+        zh: "获取你的ID, 并发起新的对话",
+        en: "Get your ID and start a new conversation."
+    },
+    command_commandHandlers_img: {
+        zh: "生成一张图片, 命令完整格式为 `/img 图片描述`, 例如`/img 月光下的沙滩`",
+        en: "Generate an image. The complete format of the command is `/img image description`, for example `/img beach under moonlight`."
+    },
+    command_commandHandlers_version: {
+        zh: "获取当前版本号, 判断是否需要更新",
+        en: "Get the current version number and determine if an update is needed."
+    },
+    command_commandHandlers_setenv: {
+        zh: "设置用户配置，命令完整格式为 /setenv KEY=VALUE",
+        en: "Set user configuration, the complete command format is /setenv KEY=VALUE."
+    },
+    command_commandHandlers_help: {
+        zh: "",
+        en: ""
+    },
+    command_commandHandlers_help: {
+        zh: "",
+        en: ""
+    },
+    command_commandHandlers_help: {
+        zh: "",
+        en: ""
+    },
+    command_commandHandlers_help: {
+        zh: "",
+        en: ""
+    },
+    command_commandHandlers_help: {
+        zh: "",
+        en: ""
+    },
+    command_commandHandlers_help: {
+        zh: "",
+        en: ""
+    },
+    command_commandHandlers_help: {
+        zh: "",
+        en: ""
+    },
+    command_commandHandlers_help: {
+        zh: "",
+        en: ""
+    },
+
+}
